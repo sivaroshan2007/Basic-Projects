61e5f4ebde1e86bfbf44fd5687f8b0f454849f48
sivaroshan2007 <sivaroshan2024@gmail.com>
2026-06-18 21:26:43 +0530

    Add files via upload
    Palindrime on java

---
+++ Palindrome_no.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String num_str = String(x)
+        Srting reversed_str = "";
+        int i = -1
+        while(i>=-(num_str.length())){
+            reversed_str = reversed_str + num_str.charAt(-1)
+        }
+        
+    }
+}
